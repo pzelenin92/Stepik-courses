96b482b2472165152720715050f75a9e29b6fe16
pzelenin92 <pzelenin92@gmail.com>
2024-04-09 19:32:34 +0300

    finishedsome 10.1 tasks

---
+++ Solution-1/Project-1/task_10_1_3.c
@@ -0,0 +1,55 @@
+﻿/*
+
+
+Факториал.
+Напишите функцию с именем factorial, которая вычисляет факториал числа,
+переданного ей в качестве аргумента.
+
+Входные данные:
+Одно целое неотрицательное число N,(N<13)
+
+Выходные данные:
+Одно целое число, факториал исходного числа.
+
+Sample Input:
+5
+
+Sample Output:
+120
+
+
+*/
+
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+
+int task_10_1_3(void) {
+
+    int a;
+    scanf("%d", &a);
+
+    printf("%d", factorial(a));
+
+
+
+    return 0;
+}
+
+int factorial(int k) {
+    // напишите здесь тело функции factorial
+    int res = k;
+
+    if (k != 0)
+    {
+        res *= factorial(k - 1);
+    }
+    else {
+        res = 1;
+    }
+
+    return res;
+
+}
