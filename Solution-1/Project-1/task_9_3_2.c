ebfb1eded33dfa53cf808bcfc9603b13244cbf01
pzelenin92 <pzelenin92@gmail.com>
2024-04-09 18:42:33 +0300

    finished 9.3 tasks

---
+++ Solution-1/Project-1/task_9_3_2.c
@@ -0,0 +1,41 @@
+﻿/*
+Имя файла.
+Вывести полное название файла в формате
+
+имя_файла.расширение
+
+Входные данные:
+Две символьных строки, записанные каждая с новой строки. В первой строке 
+записано имя файла (не более 25 символов), а во второй -- его расширение 
+(3 символа).
+
+Выходные данные:
+Одна строка указанного формата.
+
+Sample Input:
+Stepik
+exe
+
+Sample Output:
+Stepik.exe
+*/
+
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+
+int task_9_3_2(void) {
+
+    char str1[100];
+    scanf("%s", str1);
+    char str2[5];
+    scanf("%s", str2);
+
+    strcat(str1, ".");
+    strcat(str1, str2);
+    printf("%s", str1);
+
+    return 0;
+}
---
+++ Solution-1/Project-1/tasl_9_3_4.c
@@ -0,0 +1,48 @@
+﻿/*
+
+Сравнение строк
+Даны две строки. Необходимо сравнить строки между собой с учётом регистра 
+символов.
+
+Входные данные:
+Две символьных строки, записанные каждая с новой строки. Длина строк не 
+более 50 символов.
+
+Выходные данные:
+yes если строки полностью совпадают, no в противном случае.
+
+Sample Input:
+
+stepik
+Stepik
+
+Sample Output:
+no
+
+*/
+
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+
+int task_9_3_4(void) {
+
+    char str1[100];
+    scanf("%s", str1);
+
+    char str2[100];
+    scanf("%s", str2);
+
+
+    if (strcmp(str1, str2)==0) {
+        printf("yes");
+    }
+    else {
+        printf("no");
+    }
+
+
+    return 0;
+}
