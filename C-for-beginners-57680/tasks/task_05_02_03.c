f6adcfcc37b8d4ca43c141cb1f130d046d75afa0
pzelenin92 <pzelenin92@gmail.com>
2025-02-11 00:38:59 +0300

    done  task_05_02_03.c task_05_02_06.c task_05_02_07.c task_05_02_08.c

--- C-for-beginners-57680/tasks/task_05_02_03.c
+++ C-for-beginners-57680/tasks/task_05_02_03.c
@@ -4,59 +4,45 @@
 
 #include <stdio.h>
 
-
-
 #define LEN ('z' - 'a' + 1)
 
-
 // возвращает букву letter, закодированную шифром Цезаря со сдвигом shift
-int code(int letter, int shift)
-{
-    return 'a' + (letter - 'a' + shift) % LEN;
-}
+int code(int letter, int shift) { return 'a' + (letter - 'a' + shift) % LEN; }
 
-int my_code(int letter, int shift)
-{   
+int my_code(int letter, int shift) {
     int res;
-    if (shift > 0)
-    {
+    if (shift > 0) {
         res = 'a' + (letter - 'a' + shift) % LEN;
-    }
-    else {
+    } else {
         res = 'z' - ('z' - letter - shift) % LEN;
     }
     return res;
 }
 
-
-int main()
-{
+int main() {
     // читаем по символу
     char c1;    // ASCII-коды символов
-    int shift;              // сдвиг шифра Цезаря
-
+    int shift;  // сдвиг шифра Цезаря
 
     // читаем символы и сдвиг
     scanf("%c", &c1);
     scanf("%d", &shift);
 
-    #ifdef ORIGINAL
+#ifdef ORIGINAL
     // кодируем символы
     c1 = code(c1, shift);
 
-
-
     // печатаем символы
     printf("%c\n", c1);
-    #endif
+#endif
 
-    #ifdef MY_TEST
+#ifdef MY_TEST
     // кодируем символы
     c1 = my_code(c1, shift);
 
     // печатаем символы
     printf("%c\n", c1);
-    #endif
+#endif
 
     return 0;
 }
